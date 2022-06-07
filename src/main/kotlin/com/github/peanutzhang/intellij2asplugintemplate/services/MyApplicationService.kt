61cd475d339747c5c5958e85b8803c5b86394647
GitHub Action <action@github.com>
2022-06-07 10:56:49 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/peanutzhang/intellij2asplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.peanutzhang.intellij2asplugintemplate.services
+
+import com.github.peanutzhang.intellij2asplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/peanutzhang/intellij2asplugintemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/peanutzhang/intellij2asplugintemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.peanutzhang.intellij2asplugintemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.peanutzhang.intellij2asplugintemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
